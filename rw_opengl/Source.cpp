2b5ad843931ba77f016d2af6b833eba5f3f5f308
Xan5 <michal199133@wp.pl>
2017-05-04 14:49:06 +0200

    texture

--- rw_opengl/Source.cpp
+++ rw_opengl/Source.cpp
@@ -3,19 +3,20 @@
 #include <vector>
 #include <string>
 #define GLEW_STATIC
+
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
-
+#include "SOIL2/SOIL2.h"
 #include "Shader.h"
 #include "Camera.h"
 
 using namespace std;
 
 
-int importVerticles(const char* filename, vector<float> &verti);
+int importOBJ(const char* filename, vector<float> &verti, int &count);
 
 void keyStatus(GLFWwindow *window, int key, int scancode, int action, int mode);
 void mouseStatus(GLFWwindow *window, double xPos, double yPos);
@@ -31,7 +32,7 @@ GLfloat lastY = HEIGHT / 2.0;
 bool keys[1024];
 bool firstMouse = true;
 
-glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
+glm::vec3 lightPos(5.0f, 5.0f, 5.0f);
 
 GLfloat deltaTime = 0.0f;
 GLfloat lastFrame = 0.0f;
@@ -75,12 +76,13 @@ int main()
 	Shader lightingShader("lighting.vs", "lighting.frag");
 	Shader lampShader("lamp.vs", "lamp.frag");
 
+	int count=0;
 	vector<GLfloat> verticles;
-	int mode = importVerticles("cos.obj", verticles);
+	int mode = importOBJ("cos.obj", verticles, count);
 	GLuint VBO, boxVAO, lightVAO;
-	//glGenVertexArrays(1, &boxVAO);
+	glGenVertexArrays(1, &boxVAO);
 	glGenBuffers(1, &VBO);
-	//glBindVertexArray(boxVAO);
+	glBindVertexArray(boxVAO);
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, verticles.size()* sizeof(GLfloat), &verticles.front(), GL_STATIC_DRAW);
@@ -95,22 +97,21 @@ int main()
 		glBindVertexArray(0);
 	}else if(mode == 3) {
 		// Position attribute
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid *)0);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
 		glEnableVertexAttribArray(0);
-		// Texture attribute
-		
-
+		// Texture coord attribute
+		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+		glEnableVertexAttribArray(2);
 		// Normal attribute
-		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid *)(5 * sizeof(GLfloat)));
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(5 * sizeof(float)));
 		glEnableVertexAttribArray(1);
-		glBindVertexArray(0);
 	}else if(mode == 2) {
 		// Position attribute
 		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid *)0);
 		glEnableVertexAttribArray(0);
 		// Texture attribute
-		
-
+		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+		glEnableVertexAttribArray(2);
 	}else if(mode == 1) {
 		// Position attribute
 		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid *)0);
@@ -121,17 +122,38 @@ int main()
 	}
 	
 	
-	//glGenVertexArrays(1, &lightVAO);
-	//glBindVertexArray(lightVAO);
+	glGenVertexArrays(1, &lightVAO);
+	glBindVertexArray(lightVAO);
 	
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	
-	//glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid *)0);
-	//glEnableVertexAttribArray(0);
-	//glBindVertexArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid *)0);
+	glEnableVertexAttribArray(0);
+	glBindVertexArray(0);
 	
+	// Load and create a texture
+	GLuint texture;
+
+	int width, height;
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	// Set our texture parameters
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	// Set texture filtering
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	// Load, create texture and generate mipmaps
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	unsigned char *image = SOIL_load_image("tex.jpg", &width, &height, 0, SOIL_LOAD_RGBA);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
+	glGenerateMipmap(GL_TEXTURE_2D);
+	SOIL_free_image_data(image);
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+
 	glm::mat4 projection = glm::perspective(45.0f,  (GLfloat)ScreenWidth / (GLfloat)ScreenHeight, 0.1f, 100.0f);
-	
 	// Game loop
 	while (!glfwWindowShouldClose(window))
 	{
@@ -147,11 +169,15 @@ int main()
 		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 
 		lightingShader.Use();
-		GLint objectColorLoc = glGetUniformLocation(lightingShader.Program, "objectColor");
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, texture);
+		glUniform1i(glGetUniformLocation(lightingShader.Program, "Texture1"), 0);
+
+		//GLint objectColorLoc = glGetUniformLocation(lightingShader.Program, "objectColor");
 		GLint lightColorLoc = glGetUniformLocation(lightingShader.Program, "lightColor");
 		GLint lightPosLoc = glGetUniformLocation(lightingShader.Program, "lightPos");
 		GLint viewPosLoc = glGetUniformLocation(lightingShader.Program, "viewPos");
-		glUniform3f(objectColorLoc, 1.0f, 1.0f, 1.0f);
+		//glUniform3f(objectColorLoc, 1.0f, 1.0f, 1.0f);
 		glUniform3f(lightColorLoc, 1.0f, 1.0f, 1.0f);
 		glUniform3f(lightPosLoc, lightPos.x, lightPos.y, lightPos.z);
 		glUniform3f(viewPosLoc, camera.GetPosition().x, camera.GetPosition().y, camera.GetPosition().z);
@@ -166,14 +192,14 @@ int main()
 		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
 		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
 		
-		glBindVertexArray(VBO);
+		glBindVertexArray(boxVAO);
 		glm::mat4 model;
 		//model = glm::rotate(model, (GLfloat)glfwGetTime() * 1.0f, glm::vec3(0.5f, 1.0f, 0.0f));
 		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-		glDrawArrays(GL_TRIANGLES, 0, 36);
+		glDrawArrays(GL_TRIANGLES, 0, count);
 		glBindVertexArray(0);
 
-		
+		/*
 		lampShader.Use();
 		modelLoc = glGetUniformLocation(lampShader.Program, "model");
 		viewLoc = glGetUniformLocation(lampShader.Program, "view");
@@ -186,15 +212,15 @@ int main()
 		model = glm::scale(model, glm::vec3(0.2f));
 		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
 
-		glBindVertexArray(VBO);
-		//glDrawArrays(GL_TRIANGLES, 0, 36);
+		glBindVertexArray(lightVAO);
+		glDrawArrays(GL_TRIANGLES, 0, count);
 		glBindVertexArray(0);
-
+		*/
 		glfwSwapBuffers(window);
 	}
 
-	//glDeleteVertexArrays(1, &boxVAO);
-	//glDeleteVertexArrays(1, &lightVAO);
+	glDeleteVertexArrays(1, &boxVAO);
+	glDeleteVertexArrays(1, &lightVAO);
 	glDeleteBuffers(1, &VBO);
 
 	glfwTerminate();
@@ -261,7 +287,7 @@ void mouseStatus(GLFWwindow *window, double xPos, double yPos)
 	camera.processMouseMovement(xOffset, yOffset);
 }
 
-int importVerticles(const char* filename, vector<GLfloat> &verti) {
+int importOBJ(const char* filename, vector<GLfloat> &verti, int &count) {
 		std::ifstream in(filename, ios::in);
 		if (!in)
 		{
@@ -298,6 +324,7 @@ int importVerticles(const char* filename, vector<GLfloat> &verti) {
 			}
 			else if (line.substr(0, 2) == "f ")
 			{
+				count += 3;
 				stringstream s(line.substr(2));
 				s >> f1; s >> f2; s >> f3;
 				w1 = f1.find_first_of('/', 0);
@@ -418,10 +445,7 @@ int importVerticles(const char* filename, vector<GLfloat> &verti) {
 					}
 				}
 			}
-			else if (line[0] == '#')
-			{
-				// ignoring this line
-			}
+			
 		}
 		return mode;
 }
--- rw_opengl/lighting.frag
+++ rw_opengl/lighting.frag
@@ -3,31 +3,33 @@ out vec4 color;
 
 in vec3 FragPos;
 in vec3 Normal;
+in vec2 TexCoord;
 
 uniform vec3 lightPos;
 uniform vec3 viewPos;
 uniform vec3 lightColor;
-uniform vec3 objectColor;
+
+uniform sampler2D Texture1;
+//uniform vec3 objectColor;
 
 void main()
 {
     // Ambient
-    float ambientStrength = 0.12f;
-    vec3 ambient = ambientStrength * lightColor;
+    float ambientStrength = 0.30f;
+    vec3 ambient = ambientStrength * vec3(texture(Texture1, TexCoord));
     
     // Diffuse
     vec3 norm = normalize(Normal);
     vec3 lightDir = normalize(lightPos - FragPos);
     float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = diff * lightColor;
+    vec3 diffuse = diff * lightColor * vec3(texture(Texture1, TexCoord));
     
     // Specular
     float specularStrength = 0.5f;
     vec3 viewDir = normalize(viewPos - FragPos);
     vec3 reflectDir = reflect(-lightDir, norm);
     float spec = pow(max(dot(viewDir, reflectDir), 0.0),32);
-    vec3 specular = specularStrength * spec * lightColor;
+    vec3 specular = specularStrength * spec *  lightColor * (vec3(1.0f) - vec3(texture(Texture1, TexCoord)));
     
-    vec3 result = (ambient + diffuse + specular) * objectColor;
-    color = vec4(result, 1.0f);
+	color = vec4(ambient + diffuse + specular, 1.0f);  
 }
