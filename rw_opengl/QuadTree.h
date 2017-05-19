8d4836ad3804647f8d40d11317414af49e16ffef
Xan5 <michal199133@wp.pl>
2017-05-19 09:21:38 +0200

    tree

--- rw_opengl/QuadTree.h
+++ rw_opengl/QuadTree.h
@@ -1,8 +1,8 @@
 class Node {
 public:
-	GLint level;
-	GLfloat x, y;
+	GLint level, maxLevel;
 	glm::vec3 v;
+	GLfloat offSet, side;
 	glm::mat4 translation;
 	glm::mat4 rotation;
 	GLboolean object = false;
@@ -11,78 +11,75 @@ public:
 	Node * SW;
 	Node * SE;
 public:
-	Node(GLfloat _x, GLfloat _y, GLint _level) : NW(NULL), NE(NULL), SW(NULL), SE(NULL), x(_x), y(_y), level(_level) {}
-	~Node() { delete NW, NE, SW, SE; }
-	void make4Nodes(GLfloat x, GLfloat y, GLfloat q, GLint lev) {
-		this->NW = new Node(x - q, y + q, lev + 1);
-		this->NE = new Node(x + q, y + q, lev + 1);
-		this->SW = new Node(x - q, y - q, lev + 1);
-		this->SE = new Node(x + q, y - q, lev + 1);
-	}
-	void makeMats(float angle) {
-		translation = glm::translate(translation, glm::vec3(x, 0.0f, y));
-		rotation = glm::rotate(rotation, angle, glm::vec3(0.0f, 1.0f, 0.0f));
-		v = (glm::vec3(x, 0, y));
-	}
-};
+	Node(GLfloat _x, GLfloat _y, GLint _level, GLfloat _side, GLint _maxLevel) : 
+		NW(NULL), NE(NULL), SW(NULL), SE(NULL), level(_level), side(_side), maxLevel(_maxLevel) 
+	{ v = (glm::vec3(_x, 0, _y)); }
 
-class QuadTree {
-public:
-	Node * root;
-	GLfloat side;
-	GLint maxLevel;
-	QuadTree(GLfloat _side, GLint _maxlevel): side(_side), maxLevel(_maxlevel) {
-		root = new Node(0,0,0);
-	}
-	void generateTree(Node * parent) {
-		parent->make4Nodes(parent->x, parent->y, side/(2*pow(2,parent->level+1)),parent->level);
-		if (parent->level + 1 < (maxLevel)) {
-			generateTree(parent->NW);
-			generateTree(parent->NE);
-			generateTree(parent->SW);
-			generateTree(parent->SE);
+	~Node() { delete NW, NE, SW, SE; }
+
+	void generateTree() {
+		offSet = side / (2 * pow(2, level + 1));
+		NW = new Node(v.x - offSet, v.z + offSet, level + 1, side, maxLevel);
+		NE = new Node(v.x + offSet, v.z + offSet, level + 1, side, maxLevel);
+		SW = new Node(v.x - offSet, v.z - offSet, level + 1, side, maxLevel);
+		SE = new Node(v.x + offSet, v.z - offSet, level + 1, side, maxLevel);
+		if (level + 1 < (maxLevel)) {
+			NW->generateTree();
+			NE->generateTree();
+			SW->generateTree();
+			SE->generateTree();
 		}
 	}
+
+	void makeMats(GLfloat angle) {
+		translation = glm::translate(translation, glm::vec3(v.x, 0.0f, v.z));
+		rotation = glm::rotate(rotation, angle, glm::vec3(0.0f, 1.0f, 0.0f));
+	}
+
+	void renderNodes(GLint _modelLoc, GLint _count, glm::vec3 camFront, glm::vec3 camPos, GLfloat param){
+		if (this->NW != NULL) {
+			camFront.y = 0;
+			camPos.y = 0;
+			glm::vec3 treeDir = glm::normalize(camPos - v);
+			//test widocznosci
+			if ((camPos.x >= v.x - offSet && camPos.x <= v.x + offSet &&
+				camPos.z >= v.z - offSet && camPos.z <= v.z + offSet) ||
+				dot(camFront, treeDir) <= param) {
+				NW->renderNodes(_modelLoc, _count, camFront, camPos, param / 1.2);
+				NE->renderNodes(_modelLoc, _count, camFront, camPos, param / 1.2);
+				SW->renderNodes(_modelLoc, _count, camFront, camPos, param / 1.2);
+				SE->renderNodes(_modelLoc, _count, camFront, camPos, param / 1.2);
+			}
+			
+		}
+		else {
+				if (object == 0) return;
+				glm::mat4 model;
+				model *= translation * rotation;
+				glUniformMatrix4fv(_modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+				glDrawArrays(GL_TRIANGLES, 0, _count);
+		}
+	}
+
 	int addRandomTree() {
-		Node * n = root;
-		int q = 0;
-		for (int i = 0; i < maxLevel; i++) {
-			q = rand() % 4;
+		if (NW != NULL) {
+			int q = rand() % 4;
 			switch (q)
 			{
-			case 0: {n = n->NW; break; }
-			case 1: {n = n->NE; break; }
-			case 2: {n = n->SW; break; }
-			case 3: {n = n->SE; break; }
+			case 0: {return NW->addRandomTree(); }
+			case 1: {return NE->addRandomTree(); }
+			case 2: {return SW->addRandomTree(); }
+			case 3: {return SE->addRandomTree(); }
 			default:
 				return 0;
 			}
-		}
-		if (n->object == false) {
+		}else
+		if (object == false) {
 			float a = rand() % 360;
-			n->makeMats(a);
-			n->object = true;
+			makeMats(a);
+			object = true;
 			return 1;
 		}
 		else return 0;
-
 	}
-	void renderObjects(Node * node, GLint _modelLoc, GLint _count, glm::vec3 camFront, glm::vec3 camPos) {
-		if (node->NW != NULL && node->NE != NULL && node->SW != NULL && node->SE != NULL) {
-			renderObjects(node->NW, _modelLoc, _count, camFront, camPos);
-			renderObjects(node->NE, _modelLoc, _count, camFront, camPos);
-			renderObjects(node->SW, _modelLoc, _count, camFront, camPos);
-			renderObjects(node->SE, _modelLoc, _count, camFront, camPos);
-		}else{
-			if (node->object == 0) return;
-			glm::vec3 treeDir = glm::normalize(camPos - node->v);
-			if (dot(camFront, treeDir) > -0.2) return;
-			glm::mat4 model;
-			model *= node->translation * node->rotation;
-			glUniformMatrix4fv(_modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-			glDrawArrays(GL_TRIANGLES, 0, _count);
-		}
-	}
-
-	~QuadTree() { delete root; }
 };
